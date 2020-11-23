1182d7f15e36f0a17a0d800fb407880d0e1b9bf0
Huba <huba.sz23@gmail.com>
2020-11-23 17:32:08 +0200

    Vegleges adatbazis hasznalat, johet az osszekotes.

--- src/main.py
+++ src/main.py
@@ -2,15 +2,18 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import kivy
+#import kivy
+import member_database
+import question_database
+import class_database
 
-def print_hi(name):
+#def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#if __name__ == '__main__':
+   # print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
